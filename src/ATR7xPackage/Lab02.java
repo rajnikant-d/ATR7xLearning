0f631c8af4fba143e2aeb505c134cae44f421ba8
Admin <rdorlikar5@gmai.com>
2024-07-13 13:05:12 +0530

    Data types double update

---
+++ src/ATR7xPackage/Lab02.java
@@ -0,0 +1,12 @@
+package ATR7xPackage;
+
+public class Lab02 {
+    public static void main(String[] args){
+
+        double a = 500;
+        double b = 59;
+        double c= a/b;
+        //data type double
+        System.out.println(c);
+    }
+}
