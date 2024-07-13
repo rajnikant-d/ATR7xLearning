622b1347f8bd9cffee646b97426dbc5c15f1f38b
Admin <rdorlikar5@gmai.com>
2024-07-13 12:57:02 +0530

    First Commit

---
+++ src/ATR7xPackage/lab01.java
@@ -0,0 +1,13 @@
+package ATR7xPackage;
+
+public class lab01 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+
+        int a = 90;
+        int b = 10;
+        boolean is_married = false;
+        System.out.println(a+b);
+    }
+}
