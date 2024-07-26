fded71df297dd689cf18a4e30f527522a9bdb69e
Admin <rdorlikar5@gmai.com>
2024-07-26 21:18:24 +0530

    Homework for * program

---
+++ src/ATR7xPackage/Lab4ForLoop.java
@@ -0,0 +1,11 @@
+package ATR7xPackage;
+
+public class Lab4ForLoop {
+    public static void main(String[] args) {
+
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
